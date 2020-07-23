d7fb4f433265b56f9c42df36057340e73c437e9b
Chubbray <chubbray1@gmail.com>
2020-07-23 16:00:41 -0600

    added to server.js

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const app = express();
 app.listen(3001, () => {
     console.log(`API server now on port 3001`);
 });
+
+const { note } = require("./data/note");
+app.get("/api/note", (req, res) => {
+    res.json(note);
+});
